cfc34bcaa66cd4298e6b28c5536ae201f5585926
abdellatifboukernine <31359561+davidlotfi@users.noreply.github.com>
2019-04-13 23:05:19 +0200

    Update version.py

--- version.py
+++ version.py
@@ -3,6 +3,7 @@ import tensorflow.keras
 import pandas as pd
 import sklearn as sk
 import tensorflow as tf
+import numpy as np
 
 print(f"Tensor Flow Version: {tf.__version__}")
 print(f"Keras Version: {tensorflow.keras.__version__}")
@@ -10,4 +11,5 @@ print()
 print(f"Python {sys.version}")
 print(f"Pandas {pd.__version__}")
 print(f"Scikit-Learn {sk.__version__}")
+print(f"numpy {np.__version__}")
 
