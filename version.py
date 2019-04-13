4c69bc1980401803616696698840f9835f8d3bb6
abdellatifboukernine <31359561+davidlotfi@users.noreply.github.com>
2019-04-13 21:53:42 +0200

    Add files via upload

---
+++ version.py
@@ -0,0 +1,13 @@
+import sys
+import tensorflow.keras
+import pandas as pd
+import sklearn as sk
+import tensorflow as tf
+
+print(f"Tensor Flow Version: {tf.__version__}")
+print(f"Keras Version: {tensorflow.keras.__version__}")
+print()
+print(f"Python {sys.version}")
+print(f"Pandas {pd.__version__}")
+print(f"Scikit-Learn {sk.__version__}")
+
